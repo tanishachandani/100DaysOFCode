58c1b77a7e1d48245fcce278b1cdbec6d4c3a2c1
Tanisha Chandani <104519436+tanishachandani@users.noreply.github.com>
2023-01-14 23:21:11 +0530

    Update basics.js

--- Day1/basics.js
+++ Day1/basics.js
@@ -1,11 +1,11 @@
 // user input
 // promt()
 //
-// bill = Number(prompt("How much was your bill?"))
-// tip = Number(prompt("tip %?"))
-// tipAmount = (tip*bill)/100
-// total = bill + tipAmount
-// alert("Total: ",total)
+bill = Number(prompt("How much was your bill?"))
+tip = Number(prompt("tip %?"))
+tipAmount = (tip*bill)/100
+total = bill + tipAmount
+alert("Total: ",total)
 
 
 // Math.methods
@@ -19,21 +19,21 @@
 
 
 // FUNCTIONS:
-// function greet(name){
-//     console.log("Hey", name)
-//     console.log("Hey " + name)
-// }
-// greet('Qazi')
-// greet('Tanisha')
+function greet(name){
+    console.log("Hey", name)
+    console.log("Hey " + name)
+}
+greet('Qazi')
+greet('Tanisha')
 
 
 // TEMPLATE LITERALS
 // use backticks for a string `` and then whatever you put inside of a dollar sign and curly braces: ${} will be treated as a actual javascript inside of a string
-// function greet(user){
-// console.log(`Hey, ${user}! How have you been?`) 
-// }
-// greet('Tanisha')
-// greet("Brian") 
+function greet(user){
+console.log(`Hey, ${user}! How have you been?`) 
+}
+greet('Tanisha')
+greet("Brian") 
 
 
 // ARROW FUNCTIONS =>
@@ -41,11 +41,11 @@
 // two options: implicit return, explicit return
 //
 // IMPLICIT RETURN: no need of return and {}, it'll automatically return a+b
-// const sumA = (a,b) => a+b              
-// console.log(sumA(9,5)) 
+const sumA = (a,b) => a+b              
+console.log(sumA(9,5)) 
 //
 // EXPLICIT RETURN: similar to regular function, syntax differs
-// const sum = (a,b) => {
-//     return a+b
-// }
-// console.log(sum(1,5))  
+const sum = (a,b) => {
+    return a+b
+}
+console.log(sum(1,5))  
