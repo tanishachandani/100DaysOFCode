1f3c3ccdccf1353f8c1bf16743cb0b4c9fb10616
Tanisha Chandani <104519436+tanishachandani@users.noreply.github.com>
2023-01-22 23:25:23 +0530

    Add files via upload

---
+++ Day 9/DOM.js
@@ -0,0 +1,39 @@
+// DOM Manipulation
+
+// document.firstElementChild;
+// HTML Root of web page
+// document.firstElementChild.firstElementChild;
+// <head>
+// document.firstElementChild.lastElementChild;
+// <body>
+
+
+// GetElementBy
+// document.getElementsByTagName(< tag >);
+// document.getElementsByClassName(< className >);
+// document.getElementById(< id >);
+
+
+// Query Selector (More complex and precise selector)
+
+// document.querySelector(< CSS Selector >);
+// Returns a singular html element
+// If the query selector obtains multiple elements, the querySelector only returns the first element
+
+// document.querySelectorAll( < CSS Selector > );
+// Returns a list of all the html elements that match the CSS Selector
+
+// <selected>.style.<property> = “<new value></new>
+
+// Text Manipulation and the Text Content Property
+// <Selected>.innerHTML
+
+
+// Manipulating HTML Element Attributes
+// Attributes consist of things like, class=, href=, src=, …
+
+// <Selected>.attributes
+// Returns the mapping of all the attributes
+
+// <Selected>.getAttribute(“ < Attribute to get > ”);
+// <Selected>.setAttribute(“ < Which attribute to change> ”, “ < Value to change the attribute to > ”);
